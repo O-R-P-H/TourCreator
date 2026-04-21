d7709d23aef68eb6c3c30cc865d4e95485b7f0e0
ORPH <hex2152@gmail.com>
2026-04-21 17:07:06 +0300

    setup network

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
 // Конфигурация API - измени URL на свой при деплое
 export const config = {
-    apiUrl: 'http://localhost:3000' // Замени на https://api.yourdomain.com при деплое
+    apiUrl: 'https://backend-tour-creator.tsukawa.ru' // Замени на https://api.yourdomain.com при деплое
 };
